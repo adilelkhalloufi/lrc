98d5fbbb61566e3e6bf3a58e3848c166d0d4ffcf
adil elkhalloufi <adilelkhalloufi@gmail.com>
2024-12-26 16:28:18 +0100

    stable

--- src/Commands/CommandConstants.php
+++ src/Commands/CommandConstants.php
@@ -1,6 +1,5 @@
 <?php
 
-
 declare(strict_types=1);
 
 namespace Lrc\Commands;
@@ -11,24 +10,25 @@ use Illuminate\Support\Str;
 
 class CommandConstants extends Command
 {
-    protected $signature = 'make:constants {model?}';
+    protected $signature = 'make:constants {model?} {--fillable}';
 
     protected $description = 'Generate column constants for a given model based on its table schema';
 
     public function handle()
     {
         $modelName = $this->argument('model');
+        $isFillable = $this->option('fillable');
 
         if ($modelName === 'all') {
-            $this->processAllModels();
+            $this->processAllModels($isFillable);
         } elseif ($modelName) {
-            $this->processModel($modelName);
+            $this->processModel($modelName, $isFillable);
         } else {
             $this->error('Please specify a model or use "all" to generate constants for all models.');
         }
     }
 
-    private function processAllModels()
+    private function processAllModels(bool $isFillable)
     {
         $modelDirectory = app_path('Models');
         $modelFiles = scandir($modelDirectory);
@@ -39,17 +39,17 @@ class CommandConstants extends Command
 
         foreach ($modelFiles as $file) {
             $modelName = pathinfo($file, PATHINFO_FILENAME);
-            $this->processModel($modelName);
+            $this->processModel($modelName, $isFillable);
         }
 
         $this->info('Column constants and TABLE_NAME generated for all models.');
     }
 
-    private function processModel(string $modelName)
+    private function processModel(string $modelName, bool $isFillable)
     {
         $modelClass = "App\\Models\\{$modelName}";
 
-        if (! class_exists($modelClass)) {
+        if (!class_exists($modelClass)) {
             $this->error("Model {$modelName} does not exist.");
             return;
         }
@@ -60,8 +60,16 @@ class CommandConstants extends Command
 
         $constants = "    public const TABLE_NAME = '{$table}';\n\n";
         foreach ($columns as $column) {
-            $constantName = strtoupper('COL_'.$column);
+            $constantName = strtoupper('COL_' . $column);
             $constants .= "    public const {$constantName} = '{$column}';\n";
+        }
+
+        $fillable = '';
+        if ($isFillable) {
+            $fillableColumns = array_map(function ($column) {
+                return strtoupper('COL_' . $column);
+            }, $columns);
+            $fillable = "    public \$fillable = [" . implode(', ', $fillableColumns) . "];\n\n";
         }
 
         $modelPath = app_path("Models/{$modelName}.php");
@@ -79,12 +87,14 @@ class CommandConstants extends Command
             return;
         }
 
+        // Check if constants or fillable already exist
         if (strpos($content, 'public const TABLE_NAME') !== false && strpos($content, 'public const COL_') !== false) {
             $this->info("Constants already exist in {$modelName}. Skipping insertion.");
             return;
         }
 
-        $content = substr_replace($content, $constants, $bracePosition + 1, 0);
+        // Insert constants and fillable properties
+        $content = substr_replace($content, $constants . $fillable, $bracePosition + 1, 0);
         file_put_contents($modelPath, $content);
 
         $this->info("Column constants and TABLE_NAME generated successfully for {$modelName}.");
