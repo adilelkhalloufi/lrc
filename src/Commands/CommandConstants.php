3c33adcab62e59b44494cc2a4bdc502e301f46de
adil elkhalloufi <adilelkhalloufi@gmail.com>
2025-01-05 17:45:34 +0100

    add : delete conste from model or all

--- src/Commands/CommandConstants.php
+++ src/Commands/CommandConstants.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Str;
 
 class CommandConstants extends Command
 {
-    protected $signature = 'make:constants {model?} {--fillable}';
+    protected $signature = 'make:constants {model?} {--fillable} {--delete}';
 
     protected $description = 'Generate column constants for a given model based on its table schema';
 
@@ -18,13 +18,24 @@ class CommandConstants extends Command
     {
         $modelName = $this->argument('model');
         $isFillable = $this->option('fillable');
+        $isDelete = $this->option('delete');
 
-        if ($modelName === 'all') {
-            $this->processAllModels($isFillable);
-        } elseif ($modelName) {
-            $this->processModel($modelName, $isFillable);
+        if ($isDelete) {
+            if ($modelName === 'all') {
+                $this->deleteAllModelsConstants();
+            } elseif ($modelName) {
+                $this->deleteConstants($modelName);
+            } else {
+                $this->error('Please specify a model or use "all" to delete constants for all models.');
+            }
         } else {
-            $this->error('Please specify a model or use "all" to generate constants for all models.');
+            if ($modelName === 'all') {
+                $this->processAllModels($isFillable);
+            } elseif ($modelName) {
+                $this->processModel($modelName, $isFillable);
+            } else {
+                $this->error('Please specify a model or use "all" to generate constants for all models.');
+            }
         }
     }
 
@@ -99,4 +110,47 @@ class CommandConstants extends Command
 
         $this->info("Column constants and TABLE_NAME generated successfully for {$modelName}.");
     }
+
+    private function deleteAllModelsConstants()
+    {
+        $modelDirectory = app_path('Models');
+        $modelFiles = scandir($modelDirectory);
+
+        $modelFiles = array_filter($modelFiles, function ($file) {
+            return Str::endsWith($file, '.php') && $file !== 'Model.php';
+        });
+
+        foreach ($modelFiles as $file) {
+            $modelName = pathinfo($file, PATHINFO_FILENAME);
+            $this->deleteConstants($modelName);
+        }
+
+        $this->info('Column constants and TABLE_NAME deleted for all models.');
+    }
+
+    private function deleteConstants(string $modelName)
+    {
+        $modelClass = "App\\Models\\{$modelName}";
+
+        if (!class_exists($modelClass)) {
+            $this->error("Model {$modelName} does not exist.");
+            return;
+        }
+
+        $modelPath = app_path("Models/{$modelName}.php");
+        $content = file_get_contents($modelPath);
+
+        // Remove TABLE_NAME constant
+        $content = preg_replace("/\s*public\s+const\s+TABLE_NAME\s*=\s*'.*?';\n/", "", $content);
+
+        // Remove COL_ constants
+        $content = preg_replace("/\s*public\s+const\s+COL_.*?;\n/", "", $content);
+
+        file_put_contents($modelPath, $content);
+
+        $this->info("Column constants and TABLE_NAME deleted successfully for {$modelName}.");
+    }
+
+    // clear
+
 }
